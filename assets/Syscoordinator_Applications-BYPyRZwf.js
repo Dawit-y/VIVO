2ce1080e5406a3b4e8d49f25d3ed04d737a75b47
Dawit-y <Dawit-y@users.noreply.github.com>
2024-08-21 17:42:35 +0000

    deploy: 27dcafeb6a903c35bae588b0e1f5afc2cf6139b4

---
+++ assets/Syscoordinator_Applications-BYPyRZwf.js
@@ -0,0 +1,1 @@
+import{j as s,D as a}from"./index-CDdEqYjS.js";const i=()=>s.jsx("main",{className:"px-10 py-10",children:s.jsx("div",{className:"grid grid-cols-12 gap-6",children:s.jsx(a,{})})});export{i as default};
