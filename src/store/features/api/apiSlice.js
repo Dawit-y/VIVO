2a297c6f7cd734549ce250542db3e316dc9e5f6d
Dawit-y <dawityimer52@gmail.com>
2024-11-20 13:06:36 +0300

    change: base api url

--- src/store/features/api/apiSlice.js
+++ src/store/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://127.0.0.1:8000/",
+    baseUrl: "https://daveyimer.pythonanywhere.com/",
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.authToken?.access;
       if (token) {
